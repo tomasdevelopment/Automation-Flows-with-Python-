7c5185d35bdd84b48eaf310839d7bf5755e67275
TomasSuarez <83978153+tomasdevelopment@users.noreply.github.com>
2024-08-05 21:10:18 -0500

    Update and rename addnewcolumn_using_python_in_powerquery.py to powerquery_etl_using_python.pt

--- Power Bi + Python/NASA-NeoWs-AsteroidAnalytics/addnewcolumn_using_python_in_powerquery.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-
-dataset['Hazard_Classification'] = dataset['Is_Potentially_Hazardous'].map({True: 'Dangerous', False: 'Not Dangerous'}) #add an additional column to map if its hazardous
